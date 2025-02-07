2065e4cabe8f92cfac99d67bcfcb12efb49389b6
Anthony Borja <apborja51@gmail.com>
2025-02-06 17:28:02 -0800

    Add basic arrays for Python

---
+++ Graphing/graphing.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Declaring arrays
+a = np.array([1,2,3], dtype='int32')
+print(a)
+
+b = np.array([[9.0, 8.0, 7.0],[6.0,5.0, 4.0]])
+print(b)
+
+# Getting dimensions of arrays
+print(a.ndim)
+print(b.ndim)
+
+# Getting shape of arrays
+print(a.shape)
+print(b.shape)
+
+
+
