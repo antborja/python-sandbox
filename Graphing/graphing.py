27511755cfe6da51fc17676c37b78fc3e9952654
Anthony Borja <apborja51@gmail.com>
2025-02-07 19:10:12 -0800

    Add array access examples - 23:22

--- Graphing/graphing.py
+++ Graphing/graphing.py
@@ -16,5 +16,28 @@ print(b.ndim)
 print(a.shape)
 print(b.shape)
 
+# -----------------------------------------------
+print("\n-------------Accessing arrays-------------\n")
 
+c = np.array([[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14]])
+print(c)
 
+# Get a specific element [r, c]
+print(c[1,5])
+print(c[1,-2])
+
+# Get specific row
+print(c[0, :])
+
+# Get a specific column
+print(c[:,2])
+
+# Other functions
+print(c[0, 1:6:2])  # Subarray is inclusive beginning and exclusing ending
+
+# Modifying data
+c[0,0] = 100
+print(c)
+
+c[:,:] = 100
+print(c)
