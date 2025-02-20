cd05b1885b34d944eb58e8010142be3419e48753
Anthony Borja <apborja51@gmail.com>
2025-02-20 15:59:52 -0800

    Add complete array init section

--- Graphing/graphing.py
+++ Graphing/graphing.py
@@ -8,12 +8,12 @@ print(a)
 b = np.array([[9.0, 8.0, 7.0],[6.0,5.0, 4.0]])
 print(b)
 
-# Getting dimensions of arrays
+# Getting dimensions of arrays (2D, 3D, etc)
 print("Dimensions of arrays")
 print(a.ndim)
 print(b.ndim)
 
-# Getting shape of arrays
+# Getting shape of arrays (row, col)
 print("Getting shape of arrays")
 print(a.shape)
 print(b.shape)
@@ -35,9 +35,9 @@ print(c[0, :])
 print(c[:,2])
 
 # Other functions
-print(c[0, 1:6:2])  # Subarray is inclusive beginning and exclusing ending
+print(c[0, 1:6:2])  # Subarray is inclusive beginning and exclusing ending. Third param is for increments
 
-# Modifying data
+# Modifying data (NOTE: REFER TO EXERCISES FOR ARRAY IN ARRAY)
 c[0,0] = 100
 print(c)
 
@@ -53,7 +53,7 @@ b = np.zeros((2,2))
 print(a)
 print(b)
 
-# Initializing arrays with 1 (Special fxn)
+# Creating and initializing arrays with 1 (Special fxn)
 a = np.ones(5, dtype='int32')
 b = np.ones((2,2), dtype='int32')
 print(a)
@@ -63,8 +63,54 @@ print(b)
 a = np.full((3,3), 99, dtype='float32')
 print(a)
 
-# Initializing an array LIKE another
+# Initializing an array LIKE another. Assumes shape
 a = np.full_like(b, 100)
 print(a)
 
+# Initializing with random decimals (integers of the shape)
+a = np.random.rand(3,2)
+print(a)
 
+# Random integer (0-7)
+a = np.random.randint(7, size=(3,3))
+print(a)
+
+# Random int (4-7)
+a = np.random.randint(4,7, size=(3,3))
+print(a)
+
+# Identity matrix
+a = np.identity(3)
+print(a)
+
+# Repeating arrays
+arr = np.array([1,2,3])
+r1 = np.repeat(arr, 3)
+print(r1)
+
+arr = np.array([[1,2,3]])
+print(arr)
+r1 = np.repeat(arr, 3, axis=0)
+print(r1)
+
+# Exercise 1
+a = np.ones((5,5), dtype='int32')
+b = np.zeros((3,3), dtype='int32')
+b[1,1] = 9
+a[1:4,1:4] = b
+print (a)
+
+# Pointers
+a = np.array([1,2,3])
+b = a
+b[0] = 100
+print(b)
+print(a)
+
+a = np.array([1,2,3])
+b = a.copy()
+print(b)
+print(a)
+
+# -----------------------------------------------
+print("\n-------------MATHEMATICS-------------\n")
